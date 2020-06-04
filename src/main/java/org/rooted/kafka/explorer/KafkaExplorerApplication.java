9889c4f9e0314e02ecb0b5ae5385515bb34dc525
rootedin <seonggn.yun@gmail.com>
2020-06-04 23:03:55 +0900

    Init Project
    - Spring Boot + Vue.js : maven build configuration

---
+++ src/main/java/org/rooted/kafka/explorer/KafkaExplorerApplication.java
@@ -0,0 +1,13 @@
+package org.rooted.kafka.explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExplorerApplication.class, args);
+	}
+
+}
